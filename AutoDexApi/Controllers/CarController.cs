c5a09869b474809c6fd185414ace2b63068b6c8c
Kadu Miranda <kadumendesmiranda@gmail.com>
2025-05-19 15:10:16 -0300

    Crate GetAllCar, GetCarById, UpdateCar and DeleteCar in CarController

--- AutoDexApi/Controllers/CarController.cs
+++ AutoDexApi/Controllers/CarController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using AutoDexApi.DB;
 using AutoDexApi.Models;
 using AutoDexApi.Dtos;
+using Microsoft.EntityFrameworkCore;
 
 namespace AutoDexApi.Controllers
 {
@@ -42,6 +43,61 @@ namespace AutoDexApi.Controllers
 
             return StatusCode(201, car);
         }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Car>>> GetAllCars()
+        {
+            var cars = await _appDbContext.Cars.ToListAsync();
+
+            return Ok(cars);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Car>> GetCarById(int id)
+        {
+            var car = await _appDbContext.Cars.FindAsync(id);
+            if (car == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(car);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateCar(int id, [FromBody] CarCreateDto updatedCar)
+        {
+            var existingCar = await _appDbContext.Cars.FindAsync(id);
+
+            if (existingCar == null)
+            {
+                return NotFound("Car not found");
+            }
+
+            _appDbContext.Entry(existingCar).CurrentValues.SetValues(updatedCar);
+
+            await _appDbContext.SaveChangesAsync();
+
+            return StatusCode(201, existingCar);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCar(int id)
+        {
+            var car = await _appDbContext.Cars.FindAsync(id);
+
+            if (car == null)
+            {
+                return NotFound("Car not found");
+            }
+
+            _appDbContext.Cars.Remove(car);
+
+            await _appDbContext.SaveChangesAsync();
+
+            return StatusCode(201, car);
+        }
+
 
     }
 
